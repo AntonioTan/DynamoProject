e268157a217fafc1515103b92f51a4df67740724
AntonioTan <congmi@outlook.com>
2022-12-13 16:15:35 -0500

    add test for merkle tree

--- test/dynamo_test.exs
+++ test/dynamo_test.exs
@@ -5,4 +5,12 @@ defmodule DynamoTest do
   test "greets the world" do
     assert Dynamo.hello() == :world
   end
+
+  test "merkle tree usage" do
+    mt = MerkleTree.new ['a', 'b', 'c', 'd']
+    assert(mt.blocks == ['a', 'b', 'c', 'd'])
+    assert(MerkleTree.Crypto.hash("tendermint", :sha256)=="f6c3848fc2ab9188dd2c563828019be7cee4e269f5438c19f5173f79898e9ee6")
+    assert(MerkleTree.Crypto.sha256("tendermint")=="f6c3848fc2ab9188dd2c563828019be7cee4e269f5438c19f5173f79898e9ee6")
+  end
+
 end
